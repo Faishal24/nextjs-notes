357ddcf784ae221feddc78c097c425b0656313d7
Faishal <ishal.2401@gmail.com>
2024-07-28 19:10:55 +0700

    Add content component

--- app/page.js
+++ app/page.js
@@ -1,9 +1,42 @@
+"use client";
+
+import React from "react";
 import Sidebar from "@/components/Sidebar";
+import dynamic from "next/dynamic";
+
+const Notes = dynamic(() => import("@/components/Notes"));
+const NewNote = dynamic(() => import("@/components/NewNote"));
+const Archives = dynamic(() => import("@/components/Archives"));
+const Tags = dynamic(() => import("@/components/Tags"));
+const Settings = dynamic(() => import("@/components/Settings"));
 
 export default function Home() {
+  const [screen, setActiveScreen] = React.useState("notes");
+
+  const renderScreen = () => {
+    switch (screen) {
+      case "notes":
+        return <Notes />;
+      case "newNotes":
+        return <NewNote />;
+      case "archives":
+        return <Archives />;
+      case "tags":
+        return <Tags />;
+      case "settings":
+        return <Settings />;
+      default:
+        return <Notes />;
+    }
+  };
+
   return (
-    <main>
-      <Sidebar />
+    <main className="flex flex-row">
+      <Sidebar setActiveScreen={setActiveScreen} />
+
+      <div className="p-5 w-screen h-screen bg-white">
+        {renderScreen()}
+      </div>
     </main>
   );
 }
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -4,39 +4,39 @@ import {
   MdNotes,
   MdOutlineArchive,
   MdOutlineSettings,
-  MdOutlineTag
+  MdOutlineTag,
 } from "react-icons/md";
 
-const Sidebar = () => {
+const Sidebar = ({ setActiveScreen }) => {
   return (
-    <nav className="flex flex-col justify-between h-screen p-3 bg-gray-50 shadow w-72">
+    <nav className="flex flex-col justify-between h-screen p-3 bg-gray-50 shadow w-72 z-50">
       <ul className="space-y-2">
         <li className="rounded-md hover:bg-gray-200 transition duration-200 text-gray-800">
-          <a href="#" className="flex gap-2 items-center p-2">
+          <a href="#" className="flex gap-2 items-center p-2" onClick={() => setActiveScreen("newNotes")}>
             <MdOutlineStickyNote2 className="text-xl" />
             <span>New Note</span>
           </a>
         </li>
         <li className="rounded-md hover:bg-gray-200 transition duration-200 text-gray-800">
-          <a href="#" className="flex gap-2 items-center p-2">
+          <a href="#" className="flex gap-2 items-center p-2" onClick={() => setActiveScreen("notes")}>
             <MdNotes className="text-xl" />
             <span>Notes</span>
           </a>
         </li>
         <li className="rounded-md hover:bg-gray-200 transition duration-200 text-gray-800">
-          <a href="#" className="flex gap-2 items-center p-2">
+          <a href="#" className="flex gap-2 items-center p-2" onClick={() => setActiveScreen("archives")}>
             <MdOutlineArchive className="text-xl" />
             <span>Archives</span>
           </a>
         </li>
         <li className="rounded-md hover:bg-gray-200 transition duration-200 text-gray-800">
-          <a href="#" className="flex gap-2 items-center p-2">
+          <a href="#" className="flex gap-2 items-center p-2" onClick={() => setActiveScreen("tags")}>
             <MdOutlineTag className="text-xl" />
             <span>Tags</span>
           </a>
         </li>
         <li className="rounded-md hover:bg-gray-200 transition duration-200 text-gray-800">
-          <a href="#" className="flex gap-2 items-center p-2">
+          <a href="#" className="flex gap-2 items-center p-2" onClick={() => setActiveScreen("settings")}>
             <MdOutlineSettings className="text-xl" />
             <span>Settings</span>
           </a>
