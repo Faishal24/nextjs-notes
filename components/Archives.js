c5a67fe4ed4c4e539c986477d95869d58c8a0221
Faishal <ishal.2401@gmail.com>
2024-07-30 07:00:25 +0700

    Update archive UI

--- components/Archives.js
+++ components/Archives.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import { HiOutlineDocumentText } from "react-icons/hi2";
+import { HiOutlineDotsHorizontal } from "react-icons/hi";
 
 const Archives = () => {
   return (
-    <div>Archives</div>
-  )
-}
+    <div>
+      <h1 className="text-3xl font-semibold mb-7">Archives</h1>
 
-export default Archives
+      <ul className="flex flex-col gap-5">
+        <li className="cursor-pointer">
+          <div className="flex justify-between items-center">
+            <div className="flex gap-3">
+              <div className="bg-gray-200 rounded-md h-12 w-12 flex items-center justify-center">
+                <HiOutlineDocumentText className="text-2xl" />
+              </div>
+              <div>
+                <h3>First Archive Note</h3>
+                <p>1d ago</p>
+              </div>
+            </div>
+            <HiOutlineDotsHorizontal className="text-2xl" />
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Archives;
