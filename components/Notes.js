60d233c3c65a3e65b08705663e2bcf36ad0c8b55
Faishal <ishal.2401@gmail.com>
2024-08-14 08:13:41 +0700

    Fix small problem

--- components/Notes.js
+++ components/Notes.js
@@ -5,13 +5,13 @@ import { HiOutlineTrash } from "react-icons/hi";
 import { MdOutlineArchive } from "react-icons/md";
 import PopUp from "@/components/PopUp";
 import Logo from "@/components/Logo";
-import { useUser } from '@clerk/nextjs'
+import { useUser } from "@clerk/nextjs";
 import capitalize from "@/libs/capitalize";
 
 export default function Notes({ setActiveScreen, setData }) {
   const [notes, setNotes] = useState([]);
   const [popUpInfo, setPopUpInfo] = useState(null);
-  const { user } = useUser()
+  const { isLoaded, isSignedIn, user } = useUser();
 
   const getNotes = async () => {
     try {
@@ -65,7 +65,8 @@ export default function Notes({ setActiveScreen, setData }) {
   return (
     <div className="bg-white">
       <h1 className="text-3xl font-semibold mb-7 text-primary">
-        Welcome back, {capitalize(user.username)}
+        Welcome back,{" "}
+        {isLoaded || isSignedIn ? capitalize(user.username) : "User"}
       </h1>
       <p className="text-xl font-semibold mb-4 text-primary">
         Here are your notes:
